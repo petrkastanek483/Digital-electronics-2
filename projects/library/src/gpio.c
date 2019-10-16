b3754ce5207974848aa2f534a39e41030bdb633e
Petr Kaštánek <48011680+petrkastanek483@users.noreply.github.com>
2019-10-16 11:18:32 +0200

    Add files via upload

---
+++ projects/library/src/gpio.c
@@ -0,0 +1,45 @@
+# include "gpio.h"
+
+/* Functions - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+void GPIO_config_output ( volatile uint8_t * reg_name , uint8_t pin_num )
+{
+
+// Body of the function
+* reg_name |= _BV ( pin_num ) ;
+}
+
+/* void GPIO_config_input_nopull ( volatile uint8_t * reg_name , uint8_t pin_num )
+{
+
+// Body of the function
+* reg_name |= _BV ( pin_num ) ;
+}
+
+void GPIO_config_input_pullup ( volatile uint8_t * reg_name , uint8_t pin_num )
+{
+
+// Body of the function
+ * reg_name |= _BV ( pin_num ) ;
+} */ 
+
+void GPIO_write ( volatile uint8_t * reg_name , uint8_t pin_num , uint8_t pin_val )
+{
+
+// Body of the function
+* reg_name &= _BV ( pin_num ) ;
+* reg_name |= _BV ( pin_val ) ;
+}
+
+void GPIO_toggle ( volatile uint8_t * reg_name , uint8_t pin_num )
+{
+// Body of the function
+* reg_name ^= _BV (pin_num) ;
+}
+
+/* uint8_t GPIO_read ( volatile uint8_t * reg_name , uint8_t pin_num )
+{
+
+// Body of the function
+* reg_name |= _BV ( pin_num ) ;
+}*/
+
